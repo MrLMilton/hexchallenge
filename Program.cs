fcd57c6050eb5a31a2f170b5cefc55c21fe60f5f
Lang Hall <lhall@heckgrammar.co.uk>
2024-09-27 10:02:48 +0100

    completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection.PortableExecutable;
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 
 namespace hexchallenge
@@ -43,18 +45,46 @@ namespace hexchallenge
             }
             return valid;
         }
+        static int valueFinder(int pos, string hex, char[] hexadecimal)
+        {
+            char HexValue = Convert.ToChar(hex[pos]);
+            int position=0;
+            for (int i = 0; i < hexadecimal.Length; i++)
+            {
+                if (HexValue == hexadecimal[i])
+                {
+                    position = i;
+                }
+            } return position;
+        }
         static void Main(string[] args)
         {
             char[] hexadecimal = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            Console.WriteLine("Please enter a hex code for a colour\nThe code must start with a #\nThe number code after the # must be 6 numbers in length");
             string Hexcode = Console.ReadLine().ToUpper();
             bool hash = firstChar(Hexcode);
             bool length = lengthCheck(Hexcode);
             bool chars = charCheck(Hexcode, hexadecimal);
+            while(hash == false || length == false || chars == false) 
+            {
+                Console.WriteLine("Invalid hex code\nPlease enter a hex code for a colour\nThe code must start with a #\nThe number code after the # must be 6 numbers in length");
+                Hexcode = Console.ReadLine().ToUpper();
+                hash = firstChar(Hexcode);
+                length = lengthCheck(Hexcode);
+                if (length == true) 
+                {
+                    chars = charCheck(Hexcode, hexadecimal);
+                }
+            }
+            int Red = (valueFinder(1, Hexcode, hexadecimal) * 16) + valueFinder(2, Hexcode, hexadecimal);
+            int Green = (valueFinder(3, Hexcode, hexadecimal) * 16) + valueFinder(4, Hexcode, hexadecimal);
+            int Blue = (valueFinder(5, Hexcode, hexadecimal) * 16) + valueFinder(6, Hexcode, hexadecimal);
+
             Console.WriteLine(Hexcode);
             Console.WriteLine(hash);
             Console.WriteLine(length);
             Console.WriteLine(chars);
-            
+            Console.WriteLine($"{Red}, {Green}, {Blue}");
         }
     }
 }
